fe4fcc509c11466606715ae4b6edbc0c2ce4a407
Gisse Peralta <gisse.peralta@gmail.com>
2013-06-15 19:31:44 -0400

    Hecho hasta antes de vistas usuarios

--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,16 @@
-from principal.models import Persona
-from django.shortcuts import render_to_response
+from principal.models import Persona, historial
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response, get_object_or_404, HttpResponse
+from django.template import RequestContext
 
 def lista_personas(request):
 	personas= Persona.objects.all()
 	return render_to_response('lista_personas.html',{'lista':personas})
+
+def sobre(request):
+	html = "<html><body>Estamos experimentando</body></html>"
+	return HttpResponse(html)
+
+def inicio(request):
+	historials = historial.objects.all()
+	return render_to_response('inicio.html',{'historials':historials})
--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$','principal.views.lista_personas'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}
     ),
+    url(r'^sobre/$','principal.views.sobre'),
+    url(r'^$','principal.views.inicio'),
 )
